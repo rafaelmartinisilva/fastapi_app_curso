c012fa70687bd83cc7e6af62f0f2ed5352de47b5
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 18:28:33 -0300

    WIP: Aula 3 - Implementado os testes com o recurso de listar um usuário pelo id

--- tests/test_app.py
+++ tests/test_app.py
@@ -103,3 +103,32 @@ def test_delete_user_not_found(client):
 
     # Validar o UserPublic
     assert response.json() == {'detail': 'User not found'}
+
+
+def test_read_user(client):
+    client.post(
+        '/users/',
+        json={
+            'username': 'Rafael Martini',
+            'email': 'rafaelmartinisilva@hotmail.com',
+            'password': 'Test123',
+        },
+    )
+
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'Rafael Martini',
+        'email': 'rafaelmartinisilva@hotmail.com',
+    }
+
+
+def test_read_user_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+    assert response.json() == {'detail': 'User not found'}
