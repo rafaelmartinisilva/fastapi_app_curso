93d5521a82b51bb8ad3bde3914007f39111b29f6
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 18:04:30 -0300

    WIP: Aula 3 - Implementado a checagem se um usuário existe no "update_user"

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fast_api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
@@ -32,8 +32,12 @@ def read_users():
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
 def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
 
-    user_with_id = UserDB(id=user_id, ** user.model_dump())
+    user_with_id = UserDB(id=user_id, **user.model_dump())
 
     database[user_id - 1] = user_with_id
 
