b09edb8670a45290577303f343401e78b9695353
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 19:07:07 -0300

    WIP: Aula 4 - Modifica o teste para validar um usuário do DB que está em memória

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,15 +1,29 @@
-from fast_api.models import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_api.models import User, table_registry
 
 
 def test_create_user():
-    user = User(
-        username='Rafael Martini Silva',
-        email='rafaelmartinisilva@hotmail.com',
-        password='Test123',
+    engine = create_engine(  # Cria uma conexão com o DB em memória
+        'sqlite:///:memory:'
     )
 
+    table_registry.metadata.create_all(engine)  # Cria toda a estrtutura do DB
+
+    # Session -> camada intermediário entre o código e o DB.
+    with Session(engine) as session:
+        user = User(
+            username='Rafael Martini Silva',
+            email='rafaelmartinisilva@hotmail.com',
+            password='Test123',
+        )
+
+        session.add(user)  # adiciona o user na session
+        session.commit()  # adiociona o user no DB
+        session.refresh(user)  # atualiza o user criado com todos os dados
+
+    assert user.id == 1
     assert user.username == 'Rafael Martini Silva'
-
     assert user.email == 'rafaelmartinisilva@hotmail.com'
-
     assert user.password == 'Test123'
