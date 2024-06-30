16d3f8f9255f34e63c42acd365120cc7d340bb11
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 10:48:02 -0300

    WIP: Aula 3 - Criado um novo schema para validar usuários

--- fast_api/schemas.py
+++ fast_api/schemas.py
@@ -1,5 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
