1f06af171060efe14a5369cc3dfa19664ceffab4
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 19:21:07 -0300

    WIP: Aula 4 - Cria uma fixture no conftest.py para construír e destruir o DB e atualiza o teste do usuário selecionado

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,31 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fast_api.app import app
+from fast_api.models import table_registry
 
 
 @pytest.fixture()
 def client():
     return TestClient(app=app)
+
+
+@pytest.fixture()
+def session():
+    # Cria uma conexão com o DB em memória
+    engine = create_engine('sqlite:///:memory:')
+
+    table_registry.metadata.create_all(engine)  # Cria toda a estrtutura do DB
+
+    with Session(engine) as session:
+        # Transforma a session engine em um gerador e passa para a chamada da
+        # função de test
+        yield session
+
+    # O test acontece entre a criação do DB e sua destruição
+
+    table_registry.metadata.drop_all(engine)  # Destrói o DB após o test
+
+    return engine
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,29 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy import select
 
-from fast_api.models import User, table_registry
+from fast_api.models import User
 
 
-def test_create_user():
-    engine = create_engine(  # Cria uma conexão com o DB em memória
-        'sqlite:///:memory:'
+# session proveniente do conftest que prepara o DB
+def test_create_user(session):
+    user = User(
+        username='Rafael Martini Silva',
+        email='rafaelmartinisilva@hotmail.com',
+        password='Test123',
     )
 
-    table_registry.metadata.create_all(engine)  # Cria toda a estrtutura do DB
+    session.add(user)  # adiciona o user na session
+    session.commit()  # adiociona o user no DB
 
-    # Session -> camada intermediário entre o código e o DB.
-    with Session(engine) as session:
-        user = User(
-            username='Rafael Martini Silva',
-            email='rafaelmartinisilva@hotmail.com',
-            password='Test123',
-        )
+    # Seleciona um usuário do DB para a validação
+    result = session.scalar(
+        select(User).where(User.email == 'rafaelmartinisilva@hotmail.com')
+    )
 
-        session.add(user)  # adiciona o user na session
-        session.commit()  # adiociona o user no DB
-        session.refresh(user)  # atualiza o user criado com todos os dados
-
-    assert user.id == 1
-    assert user.username == 'Rafael Martini Silva'
-    assert user.email == 'rafaelmartinisilva@hotmail.com'
-    assert user.password == 'Test123'
+    # Valida o usuário selecionado do DB
+    assert result.id == 1
+    assert result.username == 'Rafael Martini Silva'
+    assert result.email == 'rafaelmartinisilva@hotmail.com'
+    assert result.password == 'Test123'
