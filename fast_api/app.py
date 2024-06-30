932c09829547eb8cb25f5189259184f259822c29
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 18:28:04 -0300

    WIP: Aula 3 - Criado o recurso de listar um usuário específico pelo id

--- fast_api/app.py
+++ fast_api/app.py
@@ -56,3 +56,15 @@ def delete_user(user_id: int):
     del database[user_id - 1]
 
     return {'message': 'User deleted'}
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return database[user_id - 1]
