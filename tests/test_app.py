68ebd6a91c4a88420640608733a0fa4ac01a3cca
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-06-30 14:39:11 -0300

    WIP: Aula 3 - Cria uma fixture do pytest para implementar o DRY (don't reapeat yourself) com o client do app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,24 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_api.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+@pytest.fixture()
+def client():
+    return TestClient(app=app)
 
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
