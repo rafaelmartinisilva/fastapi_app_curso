cc5842c6b8ca0783ab93b3e31d20e78bacde4599
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-07-09 11:38:11 -0300

    WIP: Aula 5 - Comando para ignorar test em códigos que não podem ser testados

--- fast_api/database.py
+++ fast_api/database.py
@@ -6,6 +6,9 @@ from fast_api.settings import Settings
 engine = create_engine(Settings().DATABASE_URL)
 
 
-def get_session():
+# Para códigos que não podem ser coberto pelos testes, deve-se informar
+# ao pytest que ele não deve analisar o código. Isso é indicado com a
+# descrição abaixo.
+def get_session():  # pragma: no cover
     with Session(engine) as session:
         yield session
