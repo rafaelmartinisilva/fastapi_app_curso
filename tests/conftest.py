65cfce9ab2838c65cdf855db5e5e2debc4841bf0
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-07-08 22:47:26 -0300

    WIP: Aula 5 - Utiliza a injeção de dependência para alterar o DB em memória durante a execução do teste do create_user

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,34 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_api.app import app
+from fast_api.database import get_session
 from fast_api.models import table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app=app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app=app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
 def session():
     # Cria uma conexão com o DB em memória
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
 
     table_registry.metadata.create_all(engine)  # Cria toda a estrtutura do DB
 
