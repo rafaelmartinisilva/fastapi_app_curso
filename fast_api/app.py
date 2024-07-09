a703d144b0fba4e783763dde3340c6ecde08c6f3
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-07-08 22:17:16 -0300

    WIP: Aula 5 - Aplica o conceito DRY na execução da session do DB

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,12 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import select
 
+from fast_api.database import get_session
 from fast_api.models import User
 from fast_api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
-from fast_api.settings import Settings
 
 app = FastAPI()
 
@@ -20,39 +19,38 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
+    session = get_session()
 
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where(
-                (User.username == user.username) | (User.email == user.email)
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exist',
             )
-        )
 
-        if db_user:
-            if db_user.username == user.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Username already exist',
-                )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exist',
+            )
 
-            elif db_user.email == user.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Email already exist',
-                )
+    print(db_user)
 
-        print(db_user)
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
 
-        db_user = User(
-            username=user.username, email=user.email, password=user.password
-        )
+    print(db_user)
 
-        print(db_user)
-
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
 
     return db_user
 
