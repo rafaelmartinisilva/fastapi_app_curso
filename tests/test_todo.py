8cc92565f6822af8d28621c59ff49992d71af48a
Rafael Martini Silva <rafaelmartinisilva@hotmail.com>
2024-07-17 22:42:53 -0300

    WIP: Aula 9 - Criado o teste do endpoint para adicionar tarefa todo ao DB

---
+++ tests/test_todo.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+
+# ########################################################################### #
+# --- Testa a criação de uma tarefa todo
+# ########################################################################### #
+def test_create_todo(client, token):
+    response = client.post(
+        '/todos/',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'title': 'Test todo',
+            'description': 'Test todo description',
+            'state': 'draft',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'title': 'Test todo',
+        'description': 'Test todo description',
+        'state': 'draft',
+    }
